7cdd090c9ee318d8ae09755603f7d92f737fa29b
mauro <maurobilardo02@gmail.com>
2025-09-08 15:18:34 +0200

    terzo commit

--- Script/script.js
+++ Script/script.js
@@ -3,13 +3,22 @@ function cittaScelta(){
     alert(nomeCitta + " è la citta che hai scelto");
 }
 
-const pi = 3.14;
 
+
+const pi = 3.14;
 function modificaValore(){
     pi = number;
 }
 
+
+
 function stampaTemperatura(){
     let temp = document.getElementById("temperatura").value;
     document.getElementById("risultato").append(temp + " gradi");
+}
+
+
+let vuoto;
+function valoreVuoto(){
+    document.getElementById("showValue").append(vuoto)
 }
