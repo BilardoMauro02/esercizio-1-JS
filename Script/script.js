95c0099fefda16c384e05d0536423e4911536391
mauro <maurobilardo02@gmail.com>
2025-09-08 16:05:16 +0200

    calcolatrice create

--- Script/script.js
+++ Script/script.js
@@ -22,3 +22,41 @@ let vuoto;
 function valoreVuoto(){
     document.getElementById("showValue").append(vuoto)
 }
+
+let numero = 42;
+let parola = "ciao";
+let flag = false;
+let nulla = null;
+let nonDefinito;
+function tipiDati(){
+    document.getElementById("campiDati").append(typeof numero + " " + typeof parola + " " + typeof flag + " " + typeof nulla + " " + typeof nonDefinito)
+}
+
+function calcola(){
+    let numero1 = Number(document.getElementById("numero1").value);
+    let numero2 = Number(document.getElementById("numero2").value);
+    let operatore = document.getElementById("operator").value;
+    switch(operatore){
+        case "+" : 
+        let somma = numero1 + numero2;
+        document.getElementById("risultatoCalcolo").append(somma);
+        break;
+
+        case "-" : 
+        let sottrazione = numero1 - numero2;
+        document.getElementById("risultatoCalcolo").append(sottrazione);
+        break;
+
+        case "*" : 
+        let moltiplicazione = numero1 * numero2;
+        document.getElementById("risultatoCalcolo").append(moltiplicazione);
+        break;
+
+        case "/" :
+        let divisione = numero1 / numero2;
+        document.getElementById("risultatoCalcolo").append(divisione);
+        break;
+
+    }
+
+}
