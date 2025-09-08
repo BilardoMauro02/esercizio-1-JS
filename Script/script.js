4f7372aa8022448e99cc91cf6efef27863994aa5
mauro <maurobilardo02@gmail.com>
2025-09-08 15:06:13 +0200

    primo commit esercizio js

---
+++ Script/script.js
@@ -0,0 +1,10 @@
+function cittaScelta(){
+    let nomeCitta = document.getElementById("citta").value;
+    alert(nomeCitta + " è la citta che hai scelto");
+}
+
+const pi = 3.14;
+
+function modificaValore(number){
+    pi = number;
+}
